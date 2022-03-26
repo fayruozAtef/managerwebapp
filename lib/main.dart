3f80ec006931754208ad0871c9b558dec789c3bd
fayruozAtef <fayrouz.atef@gmail.com>
2022-03-26 14:43:36 +0200

    merge connection

--- lib/main.dart
+++ lib/main.dart
@@ -22,10 +22,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false ,
       home:signupmanager (),
-<<<<<<< HEAD
 
-=======
->>>>>>> tryToConnet
     );
   }
 }
