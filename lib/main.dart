0d83a5a7bd5ef5c6972e349eb0bf49f505b4d605
nada443 <77789721+nada443@users.noreply.github.com>
2022-03-26 13:21:11 +0200

    jjjj

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:managerweb/widgets/qr_create_page.dart';
 import 'signupmanager.dart';
 
 void main()  async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   runApp(const MyApp());
 }
@@ -16,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false ,
-      home:QRCreatePage (),
+      home:signupmanager(),
 
     );
   }
