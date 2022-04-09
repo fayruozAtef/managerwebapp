adf4da380f5d0155f7bc5daf7bffa773e23535ca
Nadazaki1712 <98878634+Nadazaki1712@users.noreply.github.com>
2022-04-09 09:31:11 +0200

    changeDone

--- lib/widgets/changetable.dart
+++ lib/widgets/changetable.dart
@@ -17,7 +17,8 @@ class Uploadimage extends StatefulWidget {
 }
 
 class _imagepic extends State<Uploadimage> {
-  final ImagePicker _picker = ImagePicker();
+  String? _error;
+  String? _error2;
   String _group="";
   List imagelist=[];
   final number =TextEditingController();
@@ -31,7 +32,7 @@ class _imagepic extends State<Uploadimage> {
       bucket:'storageBucket: "testfirebaseflutter-aa934.appspot.com"' );
   //String filename = result.files.single.name;
 
-  _openPicker() async{
+   _openPicker() async{
     FilePickerResult? result;
     result=await FilePicker.platform.pickFiles(
       type: FileType.image
@@ -45,195 +46,209 @@ class _imagepic extends State<Uploadimage> {
       uploadTask.whenComplete(() async {
         String image = await uploadTask.snapshot.ref.getDownloadURL();
         setState(() {
-          for(int i=0; i<uploadFile.length;i++){
-          imagelist[i]=image.toString();
-          }
+
+          imagelist.add(image);
+          print("Url :: $imagelist");
         });
       });
+      return imagelist;
     }
   }
 
   photo()async{
-    await gettable.doc('$tableno').update({
-      "image":imagelist
-    });
+
+      await gettable.doc('$tableno').update({
+        "image": imagelist
+      });
+      print("Url 222 :: $imagelist");
   }
 
-  changetable() async {
-
-     /* if(seats.text==''){
-        QuerySnapshot dbt = await gettable.where("num",isEqualTo: tableno).get();
-        dbt.docs.forEach((element) {
-          setState(() {
-            nochange=element.get('no-of-sets');
-          });
-        });
-        await gettable.doc('$tableno').update({
-          "no-of-sets":nochange
-        });
-      }else{*/
+  changenoseats() async {
         await gettable.doc('$tableno').update({
           "no-of-sets":seatsno
         });
-
-      await gettable.doc('$tableno').update({
-        "location":_group
-      });
-
     }
-
+  changloca() async {
+    await gettable.doc('$tableno').update({
+       "location":_group
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          SizedBox(height: 30,),
-          ////Table no
-          Container(
+      body:SingleChildScrollView(
+
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 30,),
+            ////Table no
+            Container(
+                child: Row(
+                  children: [
+                    buildText("Enter Table Number"),
+                    Text("**",style: TextStyle(color:Colors.red,fontSize:22 ),)
+                  ],
+                )
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(50, 10, 400, 0),
+              child: buildtableno(),
+            ),
+            SizedBox(height: 30,),
+            buildText("Enter Number of Seats"),
+            Container(
+              padding: EdgeInsets.fromLTRB(50, 10, 400, 0),
+              child: buildnoseat(),
+            ),
+            SizedBox(height: 30,),
+
+            ////Location
+            Container(
               child: Row(
                 children: [
-                  buildText("Enter Table Number"),
-                  Text("**",style: TextStyle(color:Colors.red,fontSize:22 ),)
+                  buildText(" Location Of table"),
+                  const SizedBox(width: 50,),
+                  buildlocation(),
                 ],
-              )
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(15.0, 10, 500, 0),
-            child: buildtableno(),
-          ),
-          SizedBox(height: 30,),
-          buildText("Enter Number of Seats"),
-          Container(
-            padding: EdgeInsets.fromLTRB(15.0, 10, 500, 0),
-            child: buildnoseat(),
-          ),
-          SizedBox(height: 30,),
-
-          ////Location
-          Container(
-            child: Row(
-              children: [
-                buildText(" Location Of table"),
-                const SizedBox(width: 50,),
-                buildlocation(),
-              ],
+              ),
             ),
-          ),
-          SizedBox(height: 30,),
+            SizedBox(height: 30,),
 
-          ////UploadPhoto
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: ButtonTheme(
-                    minWidth: 100.0,
-                    height: 50.0,
-                    child: RaisedButton(
-                      child: Text('Upload Image',
-                        style: TextStyle(fontWeight: FontWeight.bold,
-                            fontSize: 30),
+            ////UploadPhoto
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: ButtonTheme(
+                      minWidth: 100.0,
+                      height: 50.0,
+                      child: RaisedButton(
+                        child: Row(
+                          children: [
+                            Icon(Icons.cloud_upload_outlined,
+                            size: 50,),
+                            const SizedBox(width: 10,),
+                            Text('Upload Image',
+                              style: TextStyle(fontWeight: FontWeight.bold,
+                                  fontSize: 30),
+                            ),
+                          ],
+                        ),
+                        onPressed:() {
+                          _openPicker();
+                        },
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 40.0,vertical: 8.0),
+                        color: Color.fromRGBO(65, 189, 180, 54),
                       ),
-                      onPressed:() {
-                        _openPicker();
-                      },
-
-
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      padding: EdgeInsets.symmetric(horizontal: 40.0,vertical: 8.0),
-                      color: Color.fromRGBO(65, 189, 180, 54),
                     ),
                   ),
-                ),
-                const SizedBox(width: 50,),
-                Container(
-                  child: ButtonTheme(
-                    minWidth: 100.0,
-                    height: 50.0,
-                    child: RaisedButton(
-                      child: Text('Delete Images',
-                        style: TextStyle(fontWeight: FontWeight.bold,
-                            fontSize: 30),
+                  const SizedBox(width: 80,),
+                  Container(
+                    child: ButtonTheme(
+                      minWidth: 100.0,
+                      height: 50.0,
+                      child: RaisedButton(
+                        child: Row(
+                          children: [
+                            Icon(Icons.delete_forever,
+                              size: 50,),
+                            const SizedBox(width: 10,),
+                            Text('Delete Images',
+                              style: TextStyle(fontWeight: FontWeight.bold,
+                                  fontSize: 30),
+                            ),
+                          ],
+                        ),
+                        onPressed:() {
+                          imagelist.clear();
+                        },
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 40.0,vertical: 8.0),
+                        color: Color.fromRGBO(65, 189, 180, 54),
                       ),
-                      onPressed:() {
-                        imagelist.clear();
-                      },
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      padding: EdgeInsets.symmetric(horizontal: 40.0,vertical: 8.0),
-                      color: Color.fromRGBO(65, 189, 180, 54),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 30,),
-          Expanded(
-            child: GridView.builder(
-                itemCount: imagelist.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount:4),
-                itemBuilder: (BuildContext context,int index){
-                  return Image.network(File(imagelist[index]).path,
-                  fit: BoxFit.cover,);
-                }),
-          ),
-
-          /////Change Button
-          Container(
-            height: 120,
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(5)),
+            SizedBox(height: 30,),
+            imagelist.toString() == "[]" ? Center(child: Text(" No images Uploaded",
+              style: TextStyle(fontSize: 26, fontWeight:FontWeight.bold),)): Container(
+              height: 200,
+              child: GridView.builder(
+                  itemCount: imagelist.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount:4),
+                  itemBuilder: (BuildContext context,int index){
+                    return Image.network(File(imagelist[index]).path,);
+                  }),
             ),
+            SizedBox(height: 30,),
+            /////Change Button
+            Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(5)),
+              ),
 
-            child: Center(
-              child: ButtonTheme(
-                minWidth: 200.0,
-                height: 80.0,
-                child: RaisedButton(
-                  child:
-                  Text('Change',
-                    style: TextStyle(fontWeight: FontWeight.bold,
-                        fontSize: 30),
-                  ),
-                  onPressed:() {
-                    setState(() {
-                      tableno=int.parse(number.text) ;
-                      seatsno=int.parse(seats.text) ;
-                      changetable();
-                      photo();
-                    });
+              child: Center(
+                child: ButtonTheme(
+                  minWidth: 200.0,
+                  height: 80.0,
+                  child: RaisedButton(
+                    child:
+                    Text('Change',
+                      style: TextStyle(fontWeight: FontWeight.bold,
+                          fontSize: 30),
+                    ),
+                    onPressed:() {
+                      setState(() {
+                        /*if(number.text.length<1) {
+                          _error2 = 'Enter Number of Seats';
+                        } else {
+                          _error2 = null;
+                          changenoseats();
+                        }
+                        if(seats.text.length<1) {
+                          _error = 'Enter Number of Seats';
+                        } else {
+                          _error = null;
+                          changenoseats();
+                        }*/
+                        tableno=int.parse(number.text) ;
+                        seatsno=int.parse(seats.text) ;
+                        changenoseats();
+                        if(imagelist.toString()!="[]") {
+                          photo();
+                        }
 
-                  },
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
+                        if(_group.isNotEmpty) {
+                          changloca();
+                        }
+                      });
+
+                    },
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 40.0,vertical: 8.0),
+                    color: Color.fromRGBO(65, 189, 180, 54),
                   ),
-                  padding: EdgeInsets.symmetric(horizontal: 40.0,vertical: 8.0),
-                  color: Color.fromRGBO(65, 189, 180, 54),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ) ,
     );
   }
-  /*Future<void> selectimage() async {
-    final List<XFile>? select = await _picker.pickMultiImage();
-    if(select!.isNotEmpty){
-      imagelist.addAll(select);
-    }
-    print("list l:"+imagelist.length.toString());
-    setState(() {
-    });
-  }*/
   Widget buildText(String text)=>Container(
     padding: EdgeInsets.fromLTRB(10.0,10.0,0,5),
     child: Text(
@@ -248,8 +263,15 @@ class _imagepic extends State<Uploadimage> {
       keyboardType: TextInputType.number,
       decoration:  InputDecoration(
         hintText: 'Number of seats',
+        errorText: "Please Enter Number of Seats",
+        errorStyle: TextStyle(color: Colors.black,),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.black),
+          borderRadius:  BorderRadius.circular(15.0),
+        ),
         border: OutlineInputBorder(
           borderRadius:  BorderRadius.circular(12.0),
+          borderSide: BorderSide(color: Colors.black),
         ),
 
       ),
@@ -262,8 +284,15 @@ class _imagepic extends State<Uploadimage> {
       keyboardType: TextInputType.number,
       decoration:  InputDecoration(
         hintText: 'Enter Table no',
+        errorText: "Please Enter Table Number",
+        errorStyle: TextStyle(color: Colors.black),
+        errorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.black),
+          borderRadius:  BorderRadius.circular(15.0),
+        ),
         border: OutlineInputBorder(
           borderRadius:  BorderRadius.circular(12.0),
+          borderSide: const BorderSide(color: Colors.black),
         ),
 
       ),
